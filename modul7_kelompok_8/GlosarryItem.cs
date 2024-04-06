e075c07712957d771e7474f62d2911013f1c05ef
deborahmanik <debimanik06@gmail.com>
2024-04-06 08:57:30 +0700

    first commit

---
+++ modul7_kelompok_8/GlosarryItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_8
+{
+    internal class GlosarryItem
+    {
+    }
+}
